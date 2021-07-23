e57cb967f8d5ecf42183032bd91ab91f26db1b4c
Yani666 <yanis051989@gmail.com>
2021-07-22 21:37:41 -0500

    pruebas

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
-const baseURL = "http://localhost:3001/api"
+
 axios.defaults.withCredentials= true
+//validar si mi apliacion esta en deploy(prod)
+const iProdcution = process.env.NODE_ENV === 'production'
+// si la app esta ya en produccion ccolocara otra ruta de lo contrario usara el 
+const baseURL = iProdcution ? "https://www.yani666.herokuapp.com/api" : "http://localhost:3001/api"
 export const _api = axios.create({
     baseURL,
 })
